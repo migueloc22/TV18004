a4d037e62740cc85c7a69040b03674b856ea074a
cipetsalex <alexanderjnisa@gmail.com>
2018-06-19 19:50:04 -0500

    pie de pagina
    edición texto pie de pagina

--- template/pie_pagina.php
+++ template/pie_pagina.php
@@ -3,19 +3,28 @@
 		echo "<div class = 'row'>";
 			echo "<div class='col-md-4'><br>";
 				echo "<img  src='img/CiPetSmarca_blanco.png'>" ;
+				echo " <p align='justify'>
+						<br><B>
+							CiPetS Feeder App </B><br/>
+							Con esta aplicación Web podrás calcular la cantidad exacta que debe comer tu mascota y tener un
+							balance nutricional disminuyendo los problemas de sobrepeso y obesidad, evitando que padezcan
+							enfermedades cardiovasculares, diabetes y problemas en las articulaciones.
+						</p>" ;
 			echo "</div>";
 			echo "<div class = 'col-md-4'>";
 				echo " <h2>¿Quienes Somos?</h2>" ;
+				echo " <p align='justify'>
+							CiPetS Feeder es un emprendimiento colombiano de base tecnológica que pretende mejorar la 
+							nutrición de tu mascota a través de una alimentación programada y enfocada de forma particular. 
+						</p>" ;
 			echo "</div>";
 			echo "<div class = 'col-md-4'>";
 				echo " <h2>Contacto</h2>" ;
 				echo " <p>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						<B>Dirección de Fábrica:</B> Cra 90C # 127B - 30 Bogotá - Colombia
+						<B>Celular:</B>(+57) 313 374 63 69
+						<B>WhatsApp:</B>(+57) 305 749 69 27
+						<br><B>Email:</B>clienteserviciocol@gmail.com<br/>
 						</p>" ;
 			echo "</div>";
 		echo "</div>";
